dd859d164eefecb64bb24561a9ec195417ba98d9
AkshatJain3648 <akshatjain3648@gmail.com>
2023-11-04 22:29:45 +0530

    .

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const inputEl = document.querySelectorAll("input");
+const inputEl = document.getElementsByTagName("input");
 const label = document.querySelectorAll("label")[0];
 const labelTwo = document.querySelectorAll("label")[1];
 const nextPage = document.querySelector('#next-page-link')
@@ -12,39 +12,9 @@ function showLabelTwo() {
 
 const form = document.querySelector("form");
 
-form.addEventListener("submit", function (event) {
-  // Get all of the text input elements on the form.
-  const textInputs = document.querySelectorAll('input[type="text"]');
-
-  // Get all of the number input elements on the form.
-  const numberInputs = document.querySelectorAll('input[type="number"]');
-
-  // Check if all of the text input elements have some content.
-  for (const textInput of textInputs) {
-    if (textInput.value === "") {
-      event.preventDefault();
-      alert("Please enter some content for all of the text input fields.");
-      return;
-    }
+nextPage.addEventListener('click', () => {
+  //Check if all the input fields are filled
+  if(inputEl == ''){
+    alert(inputEl);
   }
-
-  // Check if all of the number input elements have 10 digits.
-  for (const numberInput of numberInputs) {
-    if (numberInput.value.length !== 10) {
-      event.preventDefault();
-      alert("Please enter a valid 10-digit phone number.");
-      return;
-    }
-
-    else{
-        nextPage.classList.remove('inactive-link')
-    }
-
-  }
-
-  // If all of the validation checks pass, allow the form to be submitted.
-  form.submit();
-});
-
-//For carousel
-new Splide( '#image-carousel' ).mount();
+})
